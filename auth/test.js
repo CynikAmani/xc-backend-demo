3b81f5907a6bd1d5dd5865af76ded75562b38aa1
Cynik Amani <cynikpaul619@gmail.com>
2024-08-17 16:51:21 +0200

    fixed issues

--- auth/test.js
+++ auth/test.js
@@ -1,13 +1,9 @@
 const express = require('express');
-const app = express(); // Initialize the Express app
+const router = express.Router(); // Create a router
 
 // Define a POST route
-app.post('/', (req, res) => {
+router.post('/', (req, res) => {
     res.send('Hello there, server responded');
 });
 
-// Start the server and listen on a port
-const port = 3000;
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+module.exports = router; // Export the router
