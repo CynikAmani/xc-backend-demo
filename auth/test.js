d4c970b96372372cf3dfacee0411bb9caf0a30eb
Cynik Amani <cynikpaul619@gmail.com>
2024-08-18 10:27:42 +0200

    touched return content in test end point

--- auth/test.js
+++ auth/test.js
@@ -3,7 +3,7 @@ const router = express.Router(); // Create a router
 
 // Define a POST route
 router.post('/', (req, res) => {
-    res.send('Hello there, server responded');
+    res.send('Hello there, server responded, from the internet.');
 });
 
 module.exports = router; // Export the router
